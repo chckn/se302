1a37abb732c8ecf36fb883e0432f848ef53adad7
wrwanghao <18817818958@163.com>
2015-01-10 16:11:47 +0800

    mainwindow

--- CPL/mainwindow.cpp
+++ CPL/mainwindow.cpp
@@ -79,3 +79,69 @@ void MainWindow::on_pushButton_2_clicked()
     ::close(output[0]);
     ui->ResText->setText(QString::fromStdString(str));
 }
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    int input[2], output[2];
+    pipe(input);
+    pipe(output);
+    if (fork() == 0) {
+        dup2(input[0], 0);
+        dup2(output[1], 1);
+        ::close(output[1]);
+        ::close(output[0]);
+        ::close(input[1]);
+        ::close(input[0]);
+        semantic(1);
+	puts("DONE")
+        fclose(stdout);
+        exit(0);
+    }
+    ::close(input[0]);
+    ::close(output[1]);
+    FILE *fin = fdopen(input[1], "w");
+    fputs(ui->SrcText->toPlainText().toStdString().c_str(), fin);
+    fclose(fin);
+    char buffer[256];
+    std::string str;
+    while (int r = read(output[0], buffer, 256)) {
+        if (r < 0) break;
+        str.append(buffer, r);
+    }
+    ::close(output[0]);
+    ui->ResText->setText(QString::fromStdString(str));
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    int input[2], output[2];
+    pipe(input);
+    pipe(output);
+    if (fork() == 0) {
+        dup2(input[0], 0);
+        dup2(output[1], 1);
+        ::close(output[1]);
+        ::close(output[0]);
+        ::close(input[1]);
+        ::close(input[0]);
+        assembly();
+	puts("DONE");
+        fclose(stdout);
+        exit(0);
+    }
+    ::close(input[0]);
+    ::close(output[1]);
+    FILE *fin = fdopen(input[1], "w");
+    fputs(ui->SrcText->toPlainText().toStdString().c_str(), fin);
+    fclose(fin);
+    char buffer[256];
+    std::string str;
+    while (int r = read(output[0], buffer, 256)) {
+        if (r < 0) break;
+        str.append(buffer, r);
+    }
+    ::close(output[0]);
+    ui->ResText->setText(QString::fromStdString(str));
+}
+
